42003b93456911d27aaa5987f1008a8582b00072
Levi Strange <levi.strange@clickon.co>
2019-08-26 15:36:02 +0100

    tweaked more ui stuff

--- src/js/ui.js
+++ src/js/ui.js
@@ -19,17 +19,28 @@ for(var i = 0; i < buttons.length; i++) {
 
 export const generateResult = function(result) {
     document.getElementById('computerChoice').innerHTML = 'The computer picked ' + result.playerTwo.name;
+    
     let resultMessage;
     
+    // let computerScoreCount = 0;
 
     if(result.playerOne.beats.includes(result.playerTwo.name )) {
-        resultMessage = 'You Win!'
+        let userScoreCount = 0;
+        let userScore = document.getElementById('userScore').innerHTML;
+        userScoreCount += 1;
+        console.log("user", userScoreCount)
+        userScore = userScoreCount;
+
+        resultMessage = 'You Win!';
         
     }else if(result.playerTwo.beats.includes(result.playerOne.name )) {
-        resultMessage = 'You Lose!'
+        resultMessage = 'You Lose!';
+        // let computerScore = document.getElementById('computerScore').innerHTML;
+        // computerScoreCount;
+        // console.log(computerScore);
 
     }else {
-        resultMessage = 'Tie Game!'
+        resultMessage = 'Tie Game!';
     }
 
     document.getElementById('result').innerHTML = resultMessage;
